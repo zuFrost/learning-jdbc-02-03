8f58e5e5311f7a87a9a5015496032ba625a9e9b9
Alex Frost <zufrost.a@gmail.com>
2020-05-29 01:59:25 +0300

    add new customer. CustomerDAO.java. Edit JDBCExecutor.java

--- src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
+++ src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
@@ -12,11 +12,18 @@ public class JDBCExecutor {
                 "hplussport", "postgres", "password");
         try{
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt(1));
-            }
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = new Customer();
+            customer.setFirstName("George");
+            customer.setLastName("Washington");
+            customer.setEmail("george.washington@wh.gov");
+            customer.setPhone("(555) 555-6543");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Mount Vernon");
+            customer.setState("VA");
+            customer.setZipCode("22121");
+
+            customerDAO.create(customer);
         }catch(SQLException e){
             e.printStackTrace();
         }
